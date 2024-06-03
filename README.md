595fdec99155c25ce56ed157c65132db7a525c10
Martín Valdés de León <mvdl@amazon.de>
2024-06-04 01:04:28 +0200

    feat: the extension

---
+++ README.md
@@ -0,0 +1,10 @@
+# Long-term nuclear waste warning messages for Twitter
+
+Replace Twitter.com warning about content from blocked accounts with a [more meaningful message](https://en.wikipedia.org/wiki/Long-term_nuclear_waste_warning_messages).
+
+## Running this extension
+
+1. Clone this repository.
+2. Load this directory in Chrome as an [unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+3. Use Twitter.com as normal.
+4. Profit.
---
+++ scripts/content.js
@@ -0,0 +1,23 @@
+let observer = new MutationObserver(mutations => {
+	for (let mutation of mutations) {
+		// for (let addedNode of mutation.addedNodes) {
+			// if (addedNode.nodeName === "ARTICLE") {
+				swapMessages();
+			// }
+		// }
+	}
+});
+
+observer.observe(document, { childList: true, subtree: true });
+swapMessages();
+
+function swapMessages() {
+	const blockedXPath = "//span[text()='This Post is from an account you blocked.']";
+	const iterator = document.evaluate(blockedXPath, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+
+	let span = iterator.iterateNext();
+	while (span) {
+		span.innerHTML = 'This place is not a place of honor... no highly esteemed deed is commemorated here... nothing valued is here.<br><br>What is here was dangerous and repulsive to us. This message is a warning about danger.'
+		span = iterator.iterateNext();
+	}
+}
