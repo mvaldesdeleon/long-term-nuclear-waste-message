9506b470b8cf4aae5b174054d87cc654a88f81c2
Martín Valdés de León <mvdl@amazon.de>
2024-06-05 03:59:15 +0200

    fix(script): avoid mutating the document while iterating nodes

--- scripts/content.js
+++ scripts/content.js
@@ -9,9 +9,14 @@ function swapMessages() {
   const blockedXPath = "//span[text()='This Post is from an account you blocked.']";
   const iterator = document.evaluate(blockedXPath, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
 
-  let span = iterator.iterateNext();
-  while (span) {
-    span.innerHTML = 'This place is not a place of honor... no highly esteemed deed is commemorated here... nothing valued is here.<br><br>What is here was dangerous and repulsive to us. This message is a warning about danger.'
-    span = iterator.iterateNext();
+  const spans = [];
+  let next = iterator.iterateNext();
+  while (next) {
+    spans.push(next);
+    next = iterator.iterateNext();
+  }
+
+  for (let span of spans) {
+    span.innerHTML = 'This place is not a place of honor... no highly esteemed deed is commemorated here... nothing valued is here.<br><br>What is here was dangerous and repulsive to us. This message is a warning about danger.';
   }
 }
